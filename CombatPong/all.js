19366e562d640bcc7af5a4baa5c5f2338f9e810d
Joey Caero <joeycaero@gmail.com>
2014-03-25 09:02:20 -0500

    Got button to display properly. Baller, I know :]

--- CombatPong/all.js
+++ CombatPong/all.js
@@ -218,12 +218,16 @@ var CombatPong;
             var b = document.createElement("BUTTON");
             var t = document.createTextNode("CLICK ME");
             b.appendChild(t);
+            b.style.margin = "0px";
+            b.style.padding = "0px";
             b.style.zIndex = "100";
             b.style.position = "absolute";
+            b.style.display = "block";
             b.style.top = "10px";
-            b.style.bottom = "10px";
+            b.style.left = "10px";
             b.style.marginTop = "10px";
-            b.style.margin = "0px";
+            document.body.appendChild(b);
+            //b.style.margin = "0px";
         };
         GameHostingInterface.prototype.hideButtons = function () {
         };
